8960dc48e8be5ca6367cd67d6a515a7b7044141a
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-09-19 22:34:17 +0100

    Handle analysis failure

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import run from './lib/remark-runner';
 
 /* tslint:disable:no-expression-statement*/
 
-run().then(results => {
-  results.forEach(result => {
-    process.stdout.write(`${JSON.stringify(result)}\n`);
-  });
+run().then(
+  results => {
+    results.forEach(result => {
+      process.stdout.write(`${JSON.stringify(result)}\n`);
+    });
 
-  process.exit(0);
-});
+    process.exit(0);
+  },
+  reason => {
+    process.stderr.write(`${JSON.stringify(reason)}\n`);
+    process.exit(1);
+  }
+);
 
 /* tslint:enable:no-expression-statement*/
